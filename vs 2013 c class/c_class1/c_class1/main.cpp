f73248f115c9d36b798466f796585f634ed9fe58
kyoungsu1 <112610131+kyoungsu1@users.noreply.github.com>
2022-09-14 21:22:51 +0900

    Add files via upload

---
+++ vs 2013 c class/c_class1/c_class1/main.cpp
@@ -0,0 +1,15 @@
+#include <studio.h>
+
+int main<void>
+{
+	/*
+	  첫번째 작성한 C프로그램입니다.
+	  2022.09.14
+	*/
+	printf("HELLO !  \n\n");
+	printf("i am a student. \n")
+	printf("%d\n",1234);
+	printf("%d   %d  \n", 10, 20);
+	return 1;
+}
+
